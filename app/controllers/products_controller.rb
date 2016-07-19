82920f18c209a115183381675eac1ff21942424d
Claudia McCormack <mccormack.chc@gmail.com>
2016-07-19 15:45:30 -0400

    Created Monster Shoppe app and added all_products page.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,26 @@
+class ProductsController < ApplicationController
+  def all_products
+    @all_products = Product.all
+  end
+
+  def neck_bolts
+  end
+
+  def cape
+  end
+
+  def glasses_nose
+  end
+
+  def bandages
+  end
+
+  def wolf_cane
+  end
+
+  def potion
+  end
+
+  def coffin
+  end
+end
